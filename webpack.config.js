8a7dc55cecdfd38a017e44ba8e0687a940f606cd
iom <iom@riseup.net>
2018-10-26 21:52:30 +0200

    Update versions and rework webpack cfg

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,28 @@
 var path = require('path')
 var webpack = require('webpack')
 var Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 
 module.exports = {
-  entry: ['babel-polyfill', './src/main.js'],
+  entry: {
+    'babel-polyfill': 'babel-polyfill',
+    'main': './src/main.js'
+  },
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
+    publicPath: '',
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
-      },      {
         test: /\.vue$/,
         loader: 'vue-loader',
-        options: {
-          loaders: {
-          }
-          // other vue-loader options go here
-        }
       },
       {
         test: /\.js$/,
@@ -32,20 +30,32 @@ module.exports = {
         exclude: /node_modules/
       },
       {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },
+      {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?[hash]'
+          name (file) {
+            if (process.env.NODE_ENV === 'development') {
+              return '[path][name].[ext]'
+            }
+
+            return '[hash].[ext]'
+          }
         }
       }
     ]
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': path.resolve(__dirname, './src/')
-    },
-    extensions: ['*', '.js', '.vue', '.json']
+      '@': path.resolve(__dirname, './src/'),
+      'vue$': 'vue/dist/vue.esm.js'
+    }
   },
   devServer: {
     historyApiFallback: true,
@@ -55,27 +65,27 @@ module.exports = {
   performance: {
     hints: false
   },
-  plugins: [ new Dotenv({safe: true}) ],
+  plugins: [
+    new VueLoaderPlugin(),
+    new Dotenv({safe: true}),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+    //new BundleAnalyzerPlugin()
+  ],
   devtool: '#eval-source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
+  let alias = {
+      'bootstrap-vue$': 'bootstrap-vue/dist/bootstrap-vue.esm.min.js',
+      'vue-router$': 'vue-router/dist/vue-router.esm.js',
+      'leaflet$': 'leaflet/dist/leaflet.js',
+  }
+  module.exports.mode = 'production'
+  module.exports.resolve.alias = Object.assign(module.exports.resolve.alias, alias)
   module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
+    new CompressionPlugin(),
   ])
 }
+
