cb4bfe13c34a9c49cfd427861bf492512b0fa6aa
iom <iom@riseup.net>
2018-11-01 14:39:29 +0100

    Home - rework template

--- src/Home.vue
+++ src/Home.vue
@@ -2,34 +2,37 @@
   <div>
     <navbar></navbar>
     <div class="container">
-      <h1 class="m-5 text-center">Kartographische Aktion</h1>
+      <div class="jumbotron bg-white">
+        <h1 class="display-4">Karten für die direkte Aktion</h1>
+        <p class="lead">
+          Eine Aktionskarte besteht aus einem Kartenausschnitt in dem wichtige
+          Informationen wie eine Demoroute oder interessante Orte markiert sind.
+          So eine Karte soll dir und deinen Mitstreiter*innen helfen sich am Ort
+          des Geschehens leichter zurechtzufinden.
+          <br />
+          <br />
+          Im folgenden findest bereits erstellte öffentliche Karten:
+        </p>
 
-      <p class="text-center">
-        Die Kartographische Aktion soll dir ermöglichen Karten für Demos oder
-        anderer Aktionsformen zu erstellen. Solche sogenannten Aktionskarten
-        helfen dir und deinen Mitstreiter*innen sich am Ort des Geschehens
-        zurechtzufinden.
-      </p>
-
-      <div class="row bg-light">
-        <b-card-group deck class="m-2">
-          <b-link class="col-4 my-3 p-0" :to="{name:'map', params: {id: map.id}}" v-for="map in maps">
-            <b-card
-                :title="map.name"
-                :sub-title="map.datetime + '@' + map.place"
-                :img-src="map.thumbnail"
-                img-alt="Image"
-                align="center"
-                >
-              <p class="card-text">
-                {{map.description}}
-              </p>
-            </b-card>
-          </b-link>
-        </b-card-group>
+        <div class="row ">
+          <b-card-group deck class="m-2">
+            <b-link class="col-4 my-3 p-0" :to="{name:'map', params: {id: map.id}}" v-for="map in maps">
+              <b-card
+                  :title="map.name"
+                  :sub-title="map.datetime + '@' + map.place"
+                  :img-src="map.thumbnail"
+                  img-alt="Image"
+                  align="center"
+                  >
+                <p class="card-text">
+                  {{map.description}}
+                </p>
+              </b-card>
+            </b-link>
+          </b-card-group>
+        </div>
       </div>
     </div>
-
   </div>
 </template>
 
