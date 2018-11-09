118ff79adf16db6e77cc5c32c00b06c953cb3a9b
iom <iom@riseup.net>
2018-11-09 15:14:18 +0100

    Preview - center map

--- src/maps/Preview.vue
+++ src/maps/Preview.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="container">
-    <img v-if="src" :src="src" class="img-fluid my-5" alt="Preview">
+<div class="container h-100">
+  <img v-if="src" v-on:load="loaded=true" v-show="loaded" :src="src" class="img-fluid my-5" alt="Preview">
+  <div v-if="!loaded" class="container-spinner">
+    <div class="spinner"> </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
 export default {
   props: ['model'],
+  data() {
+    return {
+      loaded: false,
+    }
+  },
+  watch: {
+    src() {
+      this.loaded = false;
+    }
+  },
   computed: {
     src () {
       if (this.model) {
