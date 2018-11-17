513579d10a1c11d5a5146baee0f2c8a406c83465
iom <iom@riseup.net>
2018-11-17 16:40:01 +0100

    Home - add new map card

--- src/Home.vue
+++ src/Home.vue
@@ -10,23 +10,42 @@
           {{$t('home.description')}}
         </p>
 
-        <div class="row ">
-          <b-card-group deck class="m-2">
-            <b-link class="col-4 my-3 p-0" :to="{name:'map', params: {id: map.id}}" v-for="map in maps" :key="map.id">
+        <div class="my-5">
+          <div v-if="loading" class="container-spinner py-5">
+            <div class="spinner"> </div>
+            <p class="text-center mt-4">
+            {{$t('Loading')}}
+            </p>
+          </div>
+        </div>
+
+        <b-card-group deck class="m-2">
+          <div class="row row-eq-height">
+            <b-link class="col-md-4 my-2" :to="{name:'map.new'}">
+              <div class="card h-100 text-center pt-1">
+                <div class="circle rounded-circle bg-primary mt-4 mx-auto">
+                  <div class="content">
+                    <span class="text-light display-4">+</span>
+                  </div>
+                </div>
+                <h4 class="card-title text-secondary pt-auto mt-auto mb-0 pb-5">
+                  {{$t("home.newMap")}}
+                </h4>
+              </div>
+            </b-link>
+            <b-link class="col-12 col-md-4 my-2" :to="{name:'map', params: {id: map.id}}" v-for="map in maps" :key="map.id">
               <b-card
                   :title="map.name"
                   :sub-title="map.datetime + '@' + map.place"
                   :img-src="map.thumbnail"
                   img-alt="Image"
                   align="center"
+                  class="h-100"
                   >
-                <p class="card-text">
-                  {{map.description}}
-                </p>
               </b-card>
             </b-link>
-          </b-card-group>
-        </div>
+          </div>
+        </b-card-group>
       </div>
     </div>
   </div>
@@ -43,12 +62,15 @@ export default {
   props: ['lang'],
   data () {
     return {
+      loading: false,
       maps: []
     }
   },
   methods: {
     async fetchMaps() {
+      this.loading = true;
       this.maps = await api.getAllMaps()
+      this.loading = false;
     }
   },
   mounted() {
@@ -56,3 +78,33 @@ export default {
   }
 }
 </script>
+
+
+<style>
+.circle {
+  position: relative;
+  width: 50%;
+  height: 0;
+  padding-top: 50%;
+}
+
+.content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+.content:before {
+  content: '';
+  vertical-align: middle;
+  display: inline-block;
+  width: 0;
+  height: 100%;
+}
+
+.content span {
+  vertical-align: middle;
+  display: inline-block;
+}
+</style>
