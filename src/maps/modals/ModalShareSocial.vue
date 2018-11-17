77201cf13096555f682cbe2a2c0f270dae9a02cc
iom <iom@riseup.net>
2018-11-17 16:49:16 +0100

    ModalShareSocial - add loading spinner

--- src/maps/modals/ModalShareSocial.vue
+++ src/maps/modals/ModalShareSocial.vue
@@ -2,27 +2,57 @@
   <b-modal
       v-bind:visible="visible"
       v-on:change="$emit('change', $event)"
-      size="md" :title="$t('navbar.share.social.title')" @ok="$emit('ok')" hide-footer>
+      size="md" :title="$t('preview.share.social.title')" @ok="$emit('ok')" hide-footer>
   <div class="container">
-    <p class="text-center">
-      <b-img v-if="model && visible" :src="model.exports.png + ':small'" class="w-100" />
-      <b-form-textarea class="m-auto rounded-0 border-top-0" :rows="8" :value="text"></b-form-textarea>
-    </p>
+      <img v-if="src" v-on:load="loaded=true" v-show="loaded" :src="src" class="img-fluid" alt="Preview">
+      <div v-if="!loaded" class="container-spinner py-5 border-top border-left border-right">
+        <div class="spinner"> </div>
+        <p class="text-center mt-4">
+        {{$t('preview.generating')}}
+        </p>
+      </div>
+      <b-form-textarea class="m-auto rounded-0 border-top-0" :rows="4" :value="text"></b-form-textarea>
   </div>
 </b-modal>
 </template>
 
 <script>
 export default {
-  name: 'navbar',
+  name: 'ModalShareSocial',
   props: ['visible', 'model', 'lang'],
   model: {
     prop: 'visible',
     event: 'change'
   },
+  data() {
+    return {
+      loaded: false,
+      showModalShareSocial: false,
+      showModalShareHTML: false,
+      src: null
+    }
+  },
+  watch: {
+    'model': 'init'
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+    console.log("init", this.model)
+      if (!this.model || !this.model.exports) {
+        return;
+      }
+
+      // svg seems too ressource intensive for clients
+      this.loaded = false;
+      this.src = this.model.exports.png + '?' + this.model.data.hash
+    }
+  },
   computed: {
     text() {
-      if (!this.model) {
+      if (!this.model || !this.model.exports) {
         return;
       }
       let params = {
