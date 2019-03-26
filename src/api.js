4f4be43ed8c648aa59ee6154a7f238c937e0f4d9
kohllisti <kohllisti@riseup.net>
2019-03-26 23:01:53 +0100

    use aktionskarten.js from npm

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import {Api} from 'aktionskarten.js'
 
-var api = new Api('https://staging.aktionskarten.org') //process.env.API_ENDPOINT)
+var api = new Api(process.env.API_ENDPOINT);
 
 export {api}
