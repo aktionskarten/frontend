195a3f6eac7701b69e1b92dafc9d4cfd337f80fd
iom <iom@riseup.net>
2019-04-22 12:03:39 +0200

    Preview - poll until img is rendered
    Check if image is available every second. It will be rendered
    automatically if it's hasn't been yet. If it's still in rendering
    process we get a 202 http status code. If we get a 200 we know our image
    is ready to fetch.

--- src/maps/Preview.vue
+++ src/maps/Preview.vue
@@ -73,17 +73,23 @@ export default {
         return;
       }
 
-      // check if img is available every second
+      // check if img is available every second (it will be rendered
+      // automatically if it's not yet rendered (in this case we a 202)
       let url = this.model.renderLink('png')
-      fetch(url, {method: 'HEAD'}).then(response => {
+      fetch(url, {
+        method: 'HEAD',
+        headers: new Headers({Accept: "application/json"})
+      }).then(response => {
         if (response.status == 200) {
           this.loaded = false;
           this.urls.png = this.model.downloadLink('png')
           this.urls.pdf = this.model.downloadLink('pdf')
           this.urls.svg = this.model.downloadLink('svg')
           this.src = this.urls.png
-        } else {
+        } else if (response.status == 202){
           setTimeout(()=>this.init(), 1000);
+        } else {
+          console.warn("not found");
         }
       });
     }
