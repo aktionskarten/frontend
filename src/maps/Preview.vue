43bccf044000da11c9cc57bfcefa50cd896e4bf4
iom <iom@riseup.net>
2019-04-26 18:50:57 +0200

    Preview - always set urls

--- src/maps/Preview.vue
+++ src/maps/Preview.vue
@@ -73,6 +73,10 @@ export default {
         return;
       }
 
+      this.urls.png = this.model.downloadLink('png')
+      this.urls.pdf = this.model.downloadLink('pdf')
+      this.urls.svg = this.model.downloadLink('svg')
+
       // check if img is available every second (it will be rendered
       // automatically if it's not yet rendered (in this case we a 202)
       let url = this.model.renderLink('png')
@@ -82,9 +86,6 @@ export default {
       }).then(response => {
         if (response.status == 200) {
           this.loaded = false;
-          this.urls.png = this.model.downloadLink('png')
-          this.urls.pdf = this.model.downloadLink('pdf')
-          this.urls.svg = this.model.downloadLink('svg')
           this.src = this.urls.png
         } else if (response.status == 202){
           setTimeout(()=>this.init(), 1000);
